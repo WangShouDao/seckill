b5de8d3e7323b68320a5cfa8ba3175698ef67fa1
WangShouDao <1248773432@qq.com>
2018-07-27 15:53:45 +0800

    update

---
+++ src/main/java/org/seckill/exception/SeckillCloseException.java
@@ -0,0 +1,18 @@
+package org.seckill.exception;
+
+/**
+ * 
+ * √Î…±πÿ±’“Ï≥££®£©
+ * @author weary
+ *
+ */
+public class SeckillCloseException extends SeckillException {
+
+	public SeckillCloseException(String message) {
+		super(message);
+	}
+	
+	public SeckillCloseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ src/main/java/org/seckill/exception/SeckillException.java
@@ -0,0 +1,19 @@
+package org.seckill.exception;
+
+/**
+ * √Î…±œ‡πÿ“Ï≥£
+ * @author weary
+ *
+ */
+public class SeckillException extends RuntimeException {
+
+	public SeckillException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public SeckillException(String message) {
+		super(message);
+	}
+
+	
+}
