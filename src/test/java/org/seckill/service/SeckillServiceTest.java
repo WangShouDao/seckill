d6871f0756832539f43ef95bdb9dac89087cdcbb
WangShouDao <1248773432@qq.com>
2018-07-29 23:14:31 +0800

    update

--- src/test/java/org/seckill/service/SeckillServiceTest.java
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -39,26 +39,38 @@ public class SeckillServiceTest {
 	}
 
 	// 测试代码完整逻辑，注意可重复执行
-		@Test
-		public void testSeckillLogic() throws Exception {
-			long id = 1001;
-			Exposer exposer = seckillService.exportSeckillUrl(id);
-			if (exposer.isExposed()) {
-				logger.info("exposer={}", exposer);
-				long phone = 13631231234L;
-				String md5 = exposer.getMd5();
-				try {
-					SeckillExecution execution = seckillService.executeSeckill(id, phone, md5);
-					logger.info("execution={}", execution);
-				} catch (RepeatKillException e) {
-					logger.error(e.getMessage());
-				} catch (SeckillCloseException e) {
-					logger.error(e.getMessage());
-				}
-			} else {
-				// 秒杀未开启
-				logger.error("exposer={}", exposer);
+	@Test
+	public void testSeckillLogic() throws Exception {
+		long id = 1001;
+		Exposer exposer = seckillService.exportSeckillUrl(id);
+		if (exposer.isExposed()) {
+			logger.info("exposer={}", exposer);
+			long phone = 13631231234L;
+			String md5 = exposer.getMd5();
+			try {
+				SeckillExecution execution = seckillService.executeSeckill(id, phone, md5);
+				logger.info("execution={}", execution);
+			} catch (RepeatKillException e) {
+				logger.error(e.getMessage());
+			} catch (SeckillCloseException e) {
+				logger.error(e.getMessage());
 			}
+		} else {
+			// 秒杀未开启
+			logger.error("exposer={}", exposer);
 		}
-
+	}
+	
+	@Test
+	public void executeSeckillProcedure() {
+		long seckillId = 1001;
+		long phone = 13680111101L;
+		Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+		if(exposer.isExposed()) {
+			String md5 = exposer.getMd5();
+			SeckillExecution execution = seckillService.executeSeckillProcedure(seckillId, phone, md5);
+			logger.info(execution.getStateInfo());
+		}
+		
+	}
 }
